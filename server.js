33dff18cd46712901b4df3c6f5273bc3f333ae68
Cesar <csr2453@outlook.es>
2024-04-02 09:05:30 -0400

    Salvando lo hecho en la chamba

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const http = require('node:http');
-const mongoose = require('mongoose');
+const express = require('express');
 const {finAvailablePort} = require('./port');
-const { log, Console } = require('node:console');
+
+const app = express();
 
 const desiredPort = process.env.PORT ?? 3000 ;
 
-const server = http.createServer(
-    ( req, res )=>{
-        res.end('Hola mundo cruel');
-    }
-);
+const mockUsers = [
+    {id:1, name:'Pablo', lastName:'Vargas'},
+    {id:2, name:'Hugo', lastName:'Lopez'},
+    {id:3, name:'Marco', lastName:'Mora'},
+];
 
-mongoose.connect("mongodb+srv://root:Abcde123@cluster0.ymmzdlc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then( ()=>console.log('Conectado MongoDB') );
+app.get('/api/users',(require, response)=>{
+    response.status(201).send( mockUsers );
+});
+app.get('/api/users/:id', (request, response)=>{
+    const parsedId = parseInt(request.params.id);
+    const user = mockUsers.findIndex( (id)=> parsedId===id );
+    console.log(user);
+})
 
 finAvailablePort(desiredPort).then( port =>{
-    server.listen( port, ()=> {
-        console.log( `Server listening on port http://localhost:${server.address().port}` );
+    app.listen( port, ()=> {
+        console.log( `Server listening on port http://localhost:${port}` );
     })
 });
