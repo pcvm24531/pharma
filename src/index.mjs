d9c80f6b09d4527b37d00d720f7c6cbe06bcd4b5
Cesar <csr2453@outlook.es>
2024-04-04 15:28:04 -0400

    Recuperando funcionalidad de validador

--- src/index.mjs
+++ src/index.mjs
@@ -39,18 +39,30 @@ const mockUsers = [
     }
 );*/
 //Ejemplo get user con query => filter, value
-app.get('/api/users', (request, response)=>{
-    const {query:{filter, value}} = request;
-    //Si el filtro y valor no han sido definidos
-    if( !filter && !value ) return response.status(200).send(mockUsers);
+app.get(
+    '/api/users',
+    query('filter')
+        .isString()
+        .notEmpty().withMessage('Must not be empty')
+        .isLength({min:2, max:20}).withMessage('Must be at least 3-10 characters')
+    ,
+    (request, response)=>{
+        
+        const result = validationResult(request);
+        console.log(result);
 
-    if( filter && value ) 
-        return response.send(
-            mockUsers.filter( (user)=> user[filter].includes(value) )
-        );
-    
-    return response.send(mockUsers);
-});
+        const {query:{filter, value}} = request;
+        //Si el filtro y valor no han sido definidos
+        if( !filter && !value ) return response.status(200).send(mockUsers);
+
+        if( filter && value ) 
+            return response.send(
+                mockUsers.filter( (user)=> user[filter].includes(value) )
+            );
+        
+        return response.send(mockUsers);
+    }
+);
 
 app.get('/api/users/:id',resolveIndexByUserId ,(request, response)=>{
     const {findUserIndex}=request;
@@ -60,13 +72,21 @@ app.get('/api/users/:id',resolveIndexByUserId ,(request, response)=>{
 });
 
 //Ejemplo Save Users
-app.post('/api/users', (request, response)=>{
-    const {body} = request;
-    
-    const newUser = { id: mockUsers[mockUsers.length-1].id+1 , ...body  }
-    mockUsers.push(newUser);
-    return response.status(200).send(mockUsers);
-});
+app.post(
+    '/api/users', 
+    checkSchema(createUserValidationSchema),
+    (request, response)=>{
+        const result = validationResult(request);
+
+        if( !result.isEmpty ) return response.status(400).send({error: result.array()})
+
+        const {body} = request;
+        
+        const newUser = { id: mockUsers[mockUsers.length-1].id+1 , ...body  }
+        mockUsers.push(newUser);
+        return response.status(200).send(mockUsers);
+    }
+);
 
 //Ejemplo actualizar informacion
 app.put('/api/users/:id', resolveIndexByUserId, (request, response)=>{
