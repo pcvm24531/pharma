e62a0278e9f4e8dc2558c26f412d16557372b3b2
Pablo Home <pcvm2453@gmail.com>
2024-05-07 22:09:16 -0400

    Conexión a DB

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,37 @@
 import express from "express";
 import {finAvailablePort} from '../port.js';
+import { mockUsers } from "./utils/constants.mjs";
+import { mongoose } from "mongoose";
+import { User } from "./mongoose/schemas/users.mjs"
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/pharma')
+    .then( ()=>console.log('DB Connected') )
+    .catch( (err)=>console.log(`Error: ${err}`) );
 
 const desiredPort = process.env.PORT ?? 3000;
 
 app.get( 
     '/api/users', 
     (request, response)=>{
-        response.status(201).send({msg:'Hello'});
+        response.status(201).send(mockUsers);
     }
+);
+
+app.get(
+    '/api/users/:id',
+    (request, response)=>{
+        const parsedId = parseInt(request.params.id);
+        //Si el id no es valido
+        if ( isNaN(parsedId) ) return request.status(400).send({msg:'Invalid request'});
+
+        //Si el id es
+        const findUser = mockUsers.find( (user) => user.id===parsedId );
+        if( !findUser ) return response.status(404).send({msg:'Empty'});
+
+        response.status(200).send(findUser);
+    },
 );
 
 finAvailablePort(desiredPort).then( port =>{
