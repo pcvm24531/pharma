0616d290dbf2ba4736b2207451e41f507ab7f3b6
Cesar <csr2453@outlook.es>
2024-05-08 12:20:35 -0400

    Salvando cambios locales UPB

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,16 @@
 import express from "express";
 import {finAvailablePort} from '../port.js';
+import { mongoose } from "mongoose";
 
 const app = express();
 
 const desiredPort = process.env.PORT ?? 3000;
+const URL = process.env.DB_URI;
+mongoose.connect(URL,{})
+    .then( ()=>{
+        console.log("DB Connected!");
+    })
+    .catch(err=>{console.log(err);});
 
 app.get( 
     '/api/users', 
